8fde32642b8a951d02504c0897d3ab8e5adf8b5a
Alexis-chata <alexizz.19.ac@gmail.com>
2024-09-25 22:35:06 -0500

    Actualiza

--- resources/views/livewire/pagos.blade.php
+++ resources/views/livewire/pagos.blade.php
@@ -1,9 +1,9 @@
 <div>
     <br />
     <p>Trabajar en la sgte. ruta de directorio</p>
-    <p>{{ str_replace("public", "resources/views/administrador/pagos", getcwd()) }}</p>
+    <p>{{ str_replace("public", "resources/views/livewire/", getcwd()) }}</p>
     <p>Archivo</p>
-    <p>{{ str_replace("public", "resources/views/administrador/pagos/pagos_index.blade.php", getcwd()) }}</p>
+    <p>{{ str_replace("public", "resources/views/livewire/pagos.blade.php", getcwd()) }}</p>
     <br>
     <h3>Trabajar dentro del div</h3>
 </div>
